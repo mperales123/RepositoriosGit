e2fa74a99c2b0c0cacda8b2210fce8a763648449
usu2 <usu2@example.com>
2021-04-05 13:54:08 +0200

    Linea añadida a la clase principal de prueba1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -1,5 +1,9 @@
 package prueba1;
 
 public class Principal {
+	public static void main (String[] args) {
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+	}
 
 }
