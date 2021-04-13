33a7c81cac5516b264c71600f6ea43dce50cf7e1
usu2 <usu2@example.com>
2021-04-13 11:33:17 +0200

    "se añade la clase cambio2 independiente dentro del paquete cambio1

---
+++ prueba1/src/cambio1/cambio2.java
@@ -0,0 +1,8 @@
+package cambio1;
+
+public class cambio2 {
+	 public static void main(String[] args){
+		 System.out.println("Clase Cambio2 en prueba1");
+		 System.out.println("Clase Cambio2 en prueba1");
+	 }
+}
